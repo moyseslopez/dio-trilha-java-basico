c5f23fd3ff1fbbf45cf7bdca7257a4992c9fcbdd
moyses <moyses_henrique@outlook.com>
2024-07-29 21:32:17 -0300

    DIO Desafio Projeto - Simulando Uma Conta Bancária Através Do Terminal/Console

---
+++ conta-banco/src/main/java/me/dio/main/ContaTerminal.java
@@ -0,0 +1,30 @@
+package me.dio.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+	
+	public static void main(String[] args) {
+
+		String message = "\nOlá [NomeCliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque.";
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Por favor, insira seu nome: ");
+		String nomeCliente = scanner.nextLine();
+		
+		System.out.println("Digite o numero da sua agencia: ");
+		String agencia = scanner.next();
+		
+		System.out.println("Digite o numero da conta bancaria: ");
+		int numero = scanner.nextInt();
+		
+		System.out.println("Informe o saldo atual de sua conta bancaria: ");
+		double saldo = scanner.nextDouble();
+		
+		System.out.println(message.replace("[NomeCliente]", nomeCliente).replace("[Agencia]", agencia).replace("[Numero]", String.valueOf(numero)).replace("[Saldo]", String.valueOf(saldo)));
+		
+		scanner.close();
+	}
+}
